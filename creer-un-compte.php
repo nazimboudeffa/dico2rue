27ef89b8fcaa30646c410fc345b3825d179a9c87
Nazim Boudeffa <boudeffa@gmail.com>
2019-11-20 01:21:27 +0100

    Working in french

--- creer-un-compte.php
+++ creer-un-compte.php
@@ -7,12 +7,12 @@ if (!isset($global['systemRootPath'])) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
-	<?php include 'includes/head.php' ?>
+	<?php include 'includes/tete.php' ?>
 </head>
 <body>
 	<?php include 'includes/revive.php' ?>
 <div id="wrapper">
-	<?php include 'includes/header.php' ?>
+	<?php include 'includes/entete.php' ?>
 <div class="clear"></div>
 <div id="content">
 <div class="left_column">
