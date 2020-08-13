33ad646e4eda7789ab352d72dbd24e7fabc6a38c
Nazim Boudeffa <boudeffa@gmail.com>
2020-08-13 19:39:11 +0200

    Changed contact page

--- contact.php
+++ contact.php
@@ -1,5 +1,67 @@
 <?php
+session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+<?php include 'includes/tete.php' ?>
+</head>
+<body>
+	<?php include 'includes/revive.php' ?>
 
-include 'includes/facebook.php';
+<div id="wrapper">
+
+	<?php include 'includes/entete.php' ?>
+
+<div class="clear"></div>
+
+
+<div id="content">
+
+
+
+<div class="left_column">
+<style>
+.tc ul{ text-indent:30px; }
+.tc ul li{ list-style-position:inside; }
+</style>
+
+<div class="tc">
+<h1>Contact</h1>
+<p>Ce lien est l'ancienne page FB <a href="https://www.facebook.com/dico2rue">https://www.facebook.com/dico2rue</a></p>
+</br>
+<p>Je sais pas si il fonctionne encore du coup j'ai juste partagé quelques liens avec des hashtag pour ne pas déranger les créateurs originaux</p>
+</br>
+<p>Le site étant un abondonware n'hésitez pas à utiliser le hashtag #dico2rue  sur Twitter ou Facebook pour faire des suggestions si j'en vois je vous ferai signe</p>
+</div>
+
+
+
+
+</div> <!-- END OF CLASS LEFT COLUMN -->
+
+<div class="right_column">
+
+	<?php
+
+	include 'includes/right.php'
+
+	?>
+
+
+</div> <!-- END OF RIGHT COLUMN -->
+<div class="clear"></div>
+</div> <!-- END OF ID CONTENT -->
+<div class="clear"></div>
+<?php
+
+include 'includes/pied.php'
 
 ?>
+</div> <!-- END OF ID WRAPPER -->
+<?php
+
+include 'includes/ganalytics.php'
+
+?>
+</html>
