96e419ec4d5d8f4b31f77f82d7fdbb6c55c02eae
nazimboudeffa <boudeffa@gmail.com>
2021-11-01 21:33:50 +0100

    Removed links to old site

--- includes/top10mots.php
+++ includes/top10mots.php
@@ -1,15 +1,15 @@
 <div class="top">
 <h1 style="color:#000;">Top 10 de tous les temps</h1>
 <ul style="position:relative; top:-10px;">
-<li>#1 <a href="http://www.dico2rue.com/dictionnaire/mot/397/miskine">Miskine</a></li>
-<li>#2 <a href="http://www.dico2rue.com/dictionnaire/mot/1805/conde">Condé</a></li>
-<li>#3 <a href="http://www.dico2rue.com/dictionnaire/mot/869/tchoin">Tchoin</a></li>
-<li>#4 <a href="http://www.dico2rue.com/dictionnaire/mot/278/four-hollandais">Four hollandais</a></li>
-<li>#5 <a href="http://www.dico2rue.com/dictionnaire/mot/1700/chiller">Chiller</a></li>
-<li>#6 <a href="http://www.dico2rue.com/dictionnaire/mot/2120/mamene">Mamène</a></li>
-<li>#7 <a href="http://www.dico2rue.com/dictionnaire/mot/824/michons">Michons</a></li>
-<li>#8 <a href="http://www.dico2rue.com/dictionnaire/mot/1795/j-ai-la-taupe-qui-guigne">J'ai la taupe qui guigne</a></li>
-<li>#9 <a href="http://www.dico2rue.com/dictionnaire/mot/279/ca-sent-le-popcorn">Ca sent le popcorn</a></li>
-<li>#10 <a href="http://www.dico2rue.com/dictionnaire/mot/436/popcorn-f5">Popcorn F5</a></li>
+<li>#1 <a href="#">Miskine</a></li>
+<li>#2 <a href="#">Condé</a></li>
+<li>#3 <a href="#">Tchoin</a></li>
+<li>#4 <a href="#">Four hollandais</a></li>
+<li>#5 <a href="#">Chiller</a></li>
+<li>#6 <a href="#">Mamène</a></li>
+<li>#7 <a href="#">Michons</a></li>
+<li>#8 <a href="#">J'ai la taupe qui guigne</a></li>
+<li>#9 <a href="#">Ca sent le popcorn</a></li>
+<li>#10 <a href="#">Popcorn F5</a></li>
 </ul>
 </div>
--- includes/top10users.php
+++ includes/top10users.php
@@ -1,14 +1,14 @@
 <div class="top">
 <h1 style="color:#000;">Top utilisateurs !</h1>
 <ul style="position:relative; top:-10px;">
-<li>#1 <a href="http://www.dico2rue.com/utilisateur/2324/DidierSuperSympa">DidierSuperSympa</a></li>
-<li>#2 <a href="http://www.dico2rue.com/utilisateur/195/thuginho">thuginho</a></li>
-<li>#3 <a href="http://www.dico2rue.com/utilisateur/1691/ColdySky">ColdySky</a></li>
-<li>#4 <a href="http://www.dico2rue.com/utilisateur/526/ramonzarate">ramonzarate</a></li>
-<li>#5 <a href="http://www.dico2rue.com/utilisateur/837/PlaneteMars13">PlaneteMars13</a></li>
-<li>#6 <a href="http://www.dico2rue.com/utilisateur/466/Jean-Eusebe">Jean-Eusebe</a></li>
-<li>#7 <a href="http://www.dico2rue.com/utilisateur/385/Garc">Garc</a></li>
-<li>#8 <a href="http://www.dico2rue.com/utilisateur/54/desgonzo">desgonzo</a></li>
-<li>#9 <a href="http://www.dico2rue.com/utilisateur/547/MisterManiak">MisterManiak</a></li>
-<li>#10 <a href="http://www.dico2rue.com/utilisateur/1982/LeSenEnY">LeSenEnY</a></li></ul>
+<li>#1 <a href="#">DidierSuperSympa</a></li>
+<li>#2 <a href="#">thuginho</a></li>
+<li>#3 <a href="#">ColdySky</a></li>
+<li>#4 <a href="#">ramonzarate</a></li>
+<li>#5 <a href="#">PlaneteMars13</a></li>
+<li>#6 <a href="#">Jean-Eusebe</a></li>
+<li>#7 <a href="#">Garc</a></li>
+<li>#8 <a href="#">desgonzo</a></li>
+<li>#9 <a href="#">MisterManiak</a></li>
+<li>#10 <a href="#">LeSenEnY</a></li></ul>
 </div>
